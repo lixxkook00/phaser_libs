649cad55168c78fb69b0fbd3ed710a50682aa156
lixxkook00 <linhlinhkook00@gmail.com>
2023-12-04 16:57:29 +0700

    feat : add search bar

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import { InputAdornment, TextField } from "@mui/material";
 import { AppView } from "components"
+import { Search as SearchIcon } from '@mui/icons-material';
 import LLCodeBlock from "components/common/LLCodeBlock"
 import CODE from "data"
 
 export default function Home() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredCode = CODE.filter(item => {
+    return item.title.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
   return (
     <AppView className="mb-20">
+      <TextField
+        // label="Search"
+        variant="outlined"
+        placeholder="Search..."
+        fullWidth
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        className="mb-2 mt-4"
+      />
+
       {
-        CODE.map((item, index) => {
+        filteredCode.map((item, index) => {
           return (
             <LLCodeBlock 
               key={index}
---
+++ src/views/Home/components/SearchBar.tsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import { Input, InputAdornment, TextField } from '@mui/material'
+// import { Search as SearchIcon } from '@mui/icons-material';
+
+// export default function SearchBar({searchQuery, setSearchQuery}) {
+//   return (
+//     <TextField
+//         // label="Search"
+//         variant="outlined"
+//         placeholder="Search..."
+//         fullWidth
+//         value={searchQuery}
+//         onChange={(e) => setSearchQuery(e.target.value)}
+//         InputProps={{
+//           startAdornment: (
+//             <InputAdornment position="start">
+//               <SearchIcon />
+//             </InputAdornment>
+//           ),
+//         }}
+//         className="mb-2 mt-4"
+//       />
+//   )
+// }
