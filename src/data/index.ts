0814c472234cde785b4637fe6a6f539b801ae379
lixxkook00 <linhlinhkook00@gmail.com>
2023-12-07 16:23:14 +0700

    data : update new data

--- src/data/index.ts
+++ src/data/index.ts
@@ -225,8 +225,192 @@ const CODE : Array<ILLCode> = [
     }
       `
     },
+    {
+      title: 'Rock Paper Scissors',
+      type: 'others',
+      code: `
+      rockPaperScissors() {
+        TweenTask.FadeIn(this.overlay);
+    
+        this.choose = ['scissors', 'rock', 'paper'];
+    
+        this.rockPaperScissorsContainer = this.add.container(341, 550).setDepth(LAYER.POPUP);
+    
+        this.rockPaperScissorsContainer.add(this.add.image(0, 0, "atlas", "popup-bg"));
+    
+        this.txt1 = this.add.image(0, -223, "atlas", "txt-youropponentpick");
+        this.rockPaperScissorsContainer.add(this.txt1);
+    
+        this.circle = this.add.image(-6, -93, "atlas", "circle");
+        this.rockPaperScissorsContainer.add(this.circle);
+    
+        this.txt2 = this.add.image(0, 61, "atlas", "txt-taketyourpicktostratrgame");
+        this.rockPaperScissorsContainer.add(this.txt2);
+    
+        this.rockPaperScissorTitle = this.add.image(0, -148, "atlas", "txt-win").setScale(0);
+        this.rockPaperScissorsContainer.add(this.rockPaperScissorTitle);
+    
+        this.scissors = this.add.sprite(-145, 184, "atlas", "scissors").setScale(0);
+        this.rock = this.add.sprite(5, 184, "atlas", "rock").setScale(0);
+        this.paper = this.add.sprite(154, 184, "atlas", "paper").setScale(0);
+        
+        this.selections = [this.scissors, this.rock, this.paper];
+        this.rockPaperScissorsContainer.add([...this.selections]);
+    
+        this.anims.create({
+          key: 'choose',
+          frames: this.anims.generateFrameNames('atlas', { frames: this.choose }),
+          frameRate: 6,
+          yoyo: true,
+          repeat: -1,
+          duration: 150,
+        });
+    
+        this.opponentChoose = this.add.sprite(-6, -93, 'atlas', 'scissors')
+            .play('choose');
+            this.rockPaperScissorsContainer.add(this.opponentChoose)
+    
+        // const hand = this.add.image(7, 267, "atlas", "hand");
+        // rockPaperScissorsContainer.add(hand);
+    
+        TweenTask.FloatIn(this.rockPaperScissorsContainer, 0, GAME_HEIGHT);
+    
+        this.selections.forEach((item, index) => {
+          TweenTask.ZoomIn(item, { delay: 500 + index*100 });
+    
+          item.setInteractive().on(Phaser.Input.Events.POINTER_DOWN, () => {
+            this.selections.forEach(itemleft => item !== itemleft && TweenTask.ZoomOut(itemleft));
+    
+            TweenTask.FadeOut([this.txt1, this.txt2])
+    
+            TweenTask.MoveTo(item, 0, 170, { scale: 1.35, onComplete: () => {
+              this.opponentChoose.stop('choose');
+      
+              this.time.delayedCall(
+                1000,
+                () => {
+                  this.determineWinner(item.frame.name ,this.opponentChoose.frame.name, item);
+                }
+              )
+            } });
+          })
+        })
+      }
+
+      determineWinner(playerChoice, computerChoice, selected) {
+        let result = null;
+    
+        if (playerChoice === computerChoice) {
+          console.log("It's a tie!");
+          result = 0;
+        } else if (
+            (playerChoice === 'rock' && computerChoice === 'scissors') ||
+            (playerChoice === 'paper' && computerChoice === 'rock') ||
+            (playerChoice === 'scissors' && computerChoice === 'paper')
+        ) {
+            console.log("Player wins!");
+            result = 1;
+        } else {
+            console.log("Computer wins!");
+            result = -1;
+        }
+    
+        this.handleResult(result, selected);
+      }
+      `
+    },
+    {
+      title: 'Heart Breathing',
+      type: 'tweens',
+      code: `
+      const fx = this.add.image(this.sys.scale.width / 2, this.sys.scale.height / 2, 'heart');
+        fx.setScale(9);
+
+        this.image = fx;
+
+        // const fx2 = fx.postFX.addBloom(0xff0000, 1, 1, 1, 6, 4);
+        const barrel = fx.preFX.addBarrel(1);
+        // barrel.amount = 2;
+        this.add.tween({
+            duration: 400,
+            repeatDelay: 1000,
+            targets: barrel,
+            ease: Phaser.Math.Easing.Elastic.InOut,
+            amount: 1.2,
+            yoyo: true,
+            repeat: -1,
+            onRepeat: () => {
+                this.sound.play("heartbeat");
+            },
+            onStart: () => {
+                this.sound.play("heartbeat");
+                this.add.tween({
+                    duration: 400,
+                    repeatDelay: 1000,
+                    targets: fx,
+                    ease: (value) => Math.round(value),
+                    scale: 10,
+                    yoyo: true,
+                    repeat: -1
+                })
+            }
+        });
+      `
+    },
+    {
+      title: 'Pixelate Amount Fx',
+      type: 'effect',
+      code: `
+      const sprite = this.add.image(400, 300, 'pic');
+
+      const fx = sprite.preFX.addPixelate(-1);
+
+      this.add.text(10, 10, '[W] Increase Pixelate\n[S] Decrease Pixelate').setResolution(window.devicePixelRatio).setShadow(2, 2);
+
+      const amount = this.add.text(790, 10, 'FX.amount: -1').setResolution(window.devicePixelRatio).setOrigin(1, 0).setShadow(2, 2);
+
+      // increase or decrease with : fx.amount++; or fx.amount--;
+
+      this.time.addEvent({
+        delay: 1000,
+        callback: () => {
+            this.tweens.addCounter({
+                from: -1,
+                to: 10,
+                duration: 2000,
+                yoyo: true,
+                repeat: -1,
+                onUpdate: (value) => {
+                  fx.amount = value.getValue();
+                    textDebug.setText("Pixel Amount:{fx.amount.toFixed(2).padStart(5, " ")}");
+                }
+            })
+        }
+    });
+      `
+    },
+    {
+      title: 'Shine Effect',
+      type: 'effect',
+      code: `
+      const card = this.add.plane(this.sys.scale.width / 2, this.sys.scale.height / 2, 'card');
+
+      card.setScale(2)
+
+      const fx = card.postFX.addShine(1, .2, 5);
+
+      this.add.tween({
+          targets: card,
+          duration: 4000,
+          repeatDelay: 800,
+          rotateY: 360,
+          repeat: -1
+      });
+      `
+    },
     // {
     //   title: 'Updating...',
+    //   type: 'others',
     //   code: `
 
     //   `
